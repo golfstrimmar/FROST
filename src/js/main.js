4bddd1c5dcc6306462cae1072c291b3c11602938
victor <ushinbox@gmail.com>
2020-06-23 15:04:11 +0300

    вброс пустых файлов

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+
+// -------------burger----------------------
+$(Document).ready(function () {
+  $(".icon-menu").click(function (event) {
+    $(this).toggleClass("active"),
+    $(".menu").toggleClass("menu_active");
+    $("body").toggleClass("lock");
+  });
+});
+
+// ==========ibg=========
+function ibg() {
+  $.each($('.ibg'), function (index, val) {
+    if ($(this).find('img').length > 0) {
+      $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+    }
+  });
+}
+
+ibg();
